98ff615c82e3a2b643fdb5dee3e1c12862185d5f
maxboyko <maxboykomax@gmail.com>
2016-09-08 23:13:30 +0000

    Update lib to use rc6

--- development/app/modules/app.module.ts
+++ development/app/modules/app.module.ts
@@ -7,16 +7,16 @@ import { FormsModule } from '@angular/forms';
 import { App } from '../components/app.component';
 import { BarsViewer } from '../components/bars-viewer.component';
 
-import { HTTP_PROVIDERS } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { ApiService } from '../services/api.service';
 import { StorageService } from '../services/storage.service';
 import { AuthService } from '../services/auth.service';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, HttpModule],
     declarations: [App, BarsViewer],
-    providers: [HTTP_PROVIDERS, ApiService, StorageService, AuthService],
+    providers: [ApiService, StorageService, AuthService],
     bootstrap: [App]
 })
 
